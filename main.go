c768292051de75e145d933d6f7d22a55937498c1
daalfox <daalfox@gmail.com>
2024-06-28 23:15:21 +0330

    Add database

--- main.go
+++ main.go
@@ -3,32 +3,44 @@ package main
 import (
 	"encoding/json"
 	"fmt"
-	"math/rand"
 	"net/http"
+
+	"gorm.io/driver/sqlite"
+	"gorm.io/gorm"
 )
 
 func main() {
-	accounts := []User{}
+	db, err := gorm.Open(sqlite.Open("gopher-bank.db"), &gorm.Config{})
+	if err != nil {
+		println(err)
+		panic("failed to connect to database")
+	}
+	db.AutoMigrate(&User{})
 
-	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
-		fmt.Fprintln(w, "Welcome to gopher bank!")
-	})
-	http.HandleFunc("GET /accounts", func(w http.ResponseWriter, r *http.Request) {
-		json.NewEncoder(w).Encode(accounts)
-	})
-	http.HandleFunc("POST /register", func(w http.ResponseWriter, r *http.Request) {
-		var user User
-		json.NewDecoder(r.Body).Decode(&user)
-
-		user.Id = rand.Intn(1000000)
-
-		accounts = append(accounts, user)
-	})
+	attachRoutes(db)
 
 	http.ListenAndServe(":8080", nil)
 }
 
 type User struct {
-	Id   int    `json:"id"`
-	Name string `json:"name"`
+	ID   int    `json:"id"`
+	Name string `json:"name" gorm:"unique"`
+}
+
+func attachRoutes(db *gorm.DB) {
+	http.HandleFunc("GET /", func(w http.ResponseWriter, r *http.Request) {
+		fmt.Fprintln(w, "Welcome to gopher bank!")
+	})
+	http.HandleFunc("GET /accounts", func(w http.ResponseWriter, r *http.Request) {
+		var users []User
+		db.Find(&users)
+		json.NewEncoder(w).Encode(users)
+	})
+	http.HandleFunc("POST /register", func(w http.ResponseWriter, r *http.Request) {
+		var user User
+
+		json.NewDecoder(r.Body).Decode(&user)
+
+		db.Create(&user)
+	})
 }
